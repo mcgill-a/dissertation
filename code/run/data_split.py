146b53443300d7de3ce84dd15c5b52bd5ccf7de9
Alex PC <31523920+Cr-ad@users.noreply.github.com>
2020-03-07 11:19:57 +0000

    Added data split for tab separated text files

---
+++ code/run/data_split.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.insert(0, '..')
+from project.utils.data_helper import read_data, split_data, save_data_text
+from project.utils.directories import Info as info
+from project.utils.parameters import params
+
+# load the data from the input file
+input_file = read_data("S:/_UNI/HP/MT/repo/code/gru_attention/data/ga-en/tab/en-ga.txt")
+
+# split the data from tab delimited
+source, target = split_data(input_file)
+
+# save the data
+save_data_text(source, info.data_output_path + "split-source.txt")
+save_data_text(target, info.data_output_path + "split-target.txt", newline=True)
+
+
