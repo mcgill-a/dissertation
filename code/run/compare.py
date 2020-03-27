c0a0fd74753f627bea588d5e61f95755a2568492
mcgill-a <40276245@live.napier.ac.uk>
2020-03-27 11:03:36 +0000

    Added comparison for vocab size

--- code/run/compare.py
+++ code/run/compare.py
@@ -2,6 +2,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+def plot_loss_scores(experiments):
+    epochs = range(1,8)
+    #epochs = range(1,len(history['train_loss'])+1)
+    x_ticks = [0,2,4,6,8,10,12,14,16,18,20]
+    y_ticks = [0, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25]
+
+    for ex in experiments:
+        plt.plot(epochs, ex['loss'], ex['color'])
+
+    #plt.plot(epochs, history['train_loss'], 'g')
+    #plt.plot(epochs, history['val_loss'], 'b')
+
+    plt.title('Baseline Vocabulary Size - Validation Loss')
+    plt.ylabel('Loss')
+    plt.yticks(y_ticks)
+    plt.xlabel('Epoch')
+    plt.xticks(x_ticks)
+    plt.legend(['4000', '5000', '7000'], loc='upper right')
+    plt.show()
+
 
 def plot_bleu_scores(experiments):
     labels = ['BLEU-1', 'BLEU-2', 'BLEU-3', 'BLEU-4']
@@ -39,9 +59,38 @@ hierarchical = {
     'color': '#581845'
 }
 
+#######################################################
+
+vocab4k = {
+    'loss': [2.17, 1.8, 1.5, 0.9, 0.75, 0.64, 0.6],
+    'color': '#355C7D',
+}
+
+vocab5k = {
+    'loss': [2.05, 1.7, 1.4, 0.85, 0.6, 0.55, 0.5],
+    'color': '#6C5B7B',
+}
+
+vocab7k = {
+    'loss': [1.7070083038582986,1.3661732525456676,1.0724885641540611,0.7679230648180397,
+    0.599556667452359, 0.5046975390357866, 0.4522073805661491],
+    'color': '#C06C84',
+}
+
+
+vocab = []
+vocab.append(vocab4k)
+vocab.append(vocab5k)
+vocab.append(vocab7k)
+
+plot_loss_scores(vocab)
+
+#######################################################
+
+
 experiments = []
 experiments.append(baseline)
 experiments.append(trivial)
 #experiments.append(hierarchical)
 
-plot_bleu_scores(experiments)
+#plot_bleu_scores(experiments)
